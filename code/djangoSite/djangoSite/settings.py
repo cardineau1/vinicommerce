ab67d7e251fabbcbef9629b68d9c5d965d1d2037
vinichou94 <vincent@casperlefantom.net>
2017-01-25 14:07:23 +0100

    Ajout app userauth, templates test.html

--- code/djangoSite/djangoSite/settings.py
+++ code/djangoSite/djangoSite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'userauth',
 ]
 
 MIDDLEWARE_CLASSES = [
---
+++ code/djangoSite/userauth/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def login(request):
+    return render(request, 'test.html')
