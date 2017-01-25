4f246cb5e71b6b0282e347d80fddf46ab16d509d
vinichou94 <vincent@casperlefantom.net>
2017-01-25 20:07:41 +0100

    Ajout template base.html et login.html

--- code/djangoSite/userauth/views.py
+++ code/djangoSite/userauth/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def login(request):
-    return render(request, 'test.html')
+    return render(request, 'login.html')
+
+
